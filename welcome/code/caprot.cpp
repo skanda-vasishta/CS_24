3c478c7e33746bdadd7eae29e752847ff9d2bac4
Skanda Vasishta <skandavasishta@ucsb.edu>
2023-04-11 18:54:31 -0700

    initial code

---
+++ welcome/code/caprot.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include<cctype>
+using namespace std;
+
+int main (int argc, char*argv[]) {
+    if (argc != 2){
+        cout<< "Incorrect amount of command line arguments." ;
+    }
+    
+    int num;
+    num = atoi(argv[1]);
+
+    string userStr;
+    getline(cin, userStr);
+    int len = userStr.size();
+    string newStr = userStr;
+
+
+    for (int i = 0; i < len; i++) {
+        if (isalpha(userStr[i])) {
+            if (isupper(userStr[i])) {
+               newStr[i] = tolower(userStr[i]);
+                if ((i+num)<len) {
+                    newStr[i+num] = toupper(userStr[i+num]);
+                } else {
+                    newStr[(i+num)%len] = toupper(userStr[(i+num)%len]);
+                }
+            } 
+        }
+    }
+
+
+
+
+
+    return 0;
+}
