2d1384911ce36cb4bfc5f66431864643d6f5cb84
Skanda Vasishta <skandavasishta@ucsb.edu>
2023-04-14 15:15:44 -0700

    function stubs

--- fibvec/FibVec.cpp
+++ fibvec/FibVec.cpp
@@ -10,5 +10,39 @@ FibVec* create_fibvec() {
   return new FibVec();
 }
 
+FibVec::FibVec(){
+  //default constructor 
+}
+
+FibVec::~FibVec(){
+  //destructor
+}
 
 // FibVec Function Implementations
+size_t FibVec::capacity() const{
+
+}
+
+ size_t FibVec::count() const {
+
+ }
+
+ void FibVec::insert(int value, int index){
+
+ }
+
+ int FibVec::lookup(int index) const {
+
+ }
+
+ int FibVec::pop(){
+
+ }
+
+void FibVec::push(int value) {
+
+}
+
+int FibVec::remove(int index){
+
+}
--- fibvec/FibVec.h
+++ fibvec/FibVec.h
@@ -11,9 +11,23 @@ class FibVec {
 
 public:
   // Constructor and Destructor
+    FibVec(); //Constructor
+    ~FibVec(); //Destructor
 
   // Member Functions
+  size_t capacity() const;
+  size_t count() const;
+  void insert(int value, int index);
+  int lookup(int index) const;
+  int pop();
+  void push(int value);
+  int remove(int index);
 
+private:
+  int *vec;
+  size_t cap;
+  size_t cnt;
+  
 };
 
 // Used by the autograder:
