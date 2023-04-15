eb928fc104a520d5d365da9d5e5677fdb2d9720d
Skanda Vasishta <skandavasishta@ucsb.edu>
2023-04-15 14:48:08 -0700

    insert

--- fibvec/FibVec.cpp
+++ fibvec/FibVec.cpp
@@ -36,7 +36,7 @@ size_t FibVec::capacity() const{
  }
 
  void FibVec::insert(int value, size_t index){
-  if (index>cnt){
+  if (index>cap){
     throw std::out_of_range("index is out of range");
   }
   if (cnt>cap){
