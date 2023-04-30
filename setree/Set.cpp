93e6295b40d04dd99d97bfc8eb44618f9789810c
Skanda Vasishta <skandavasishta@ucsb.edu>
2023-04-30 01:44:32 -0700

    set

--- setree/Set.cpp
+++ setree/Set.cpp
@@ -21,10 +21,12 @@ size_t Set::clear() {
 
 bool Set::contains(const std::string& value) const {
     //ok
+    return true;
 }
 
 size_t Set::count() const{
     //ok
+    return 1;
 }
 
 void debug(){
@@ -33,10 +35,12 @@ void debug(){
 
 size_t Set::insert(const std::string& value){
     //set
+    return 1;
 }
 
 const std::string& Set::lookup(size_t n) const{
     //set
+    return "ok";
 }
 
 void Set::print() const{
@@ -45,4 +49,5 @@ void Set::print() const{
 
 size_t Set:: remove(const std::string& value){
     //ok
+    return 1;
 }
