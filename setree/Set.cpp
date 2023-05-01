94f6a90ca4d6bd499bbffae6a68aec01276e7464
Skanda Vasishta <skandavasishta@ucsb.edu>
2023-05-01 16:27:02 -0700

    ok

--- setree/Set.cpp
+++ setree/Set.cpp
@@ -58,6 +58,37 @@ void debug(){
     //ok
 }
 
+
+size_t insert_help(const std::string& value, Node* node){
+       if (node == nullptr){
+        node = new Node;
+        node ->data = value;
+    }
+    if (node-> data == value){
+        node->count = 0;
+    } 
+    if (value < node->data){
+        if (node -> left == nullptr){
+            node->left = new Node();
+            node->left->data = value;
+        } else {
+            insert_help(value, node);
+        }
+    }
+    if (value > node->data){
+        if (node -> right == nullptr){
+            node->right = new Node();
+            node->right->data = value;
+        } else {
+            insert_help(value, node);
+
+        }
+    }
+    node->count ++;
+
+    return node->count;
+}
+
 size_t Set::insert(const std::string& value){
     if (mRoot == nullptr){
         mRoot = new Node;
@@ -71,7 +102,7 @@ size_t Set::insert(const std::string& value){
             mRoot->left = new Node();
             mRoot->left->data = value;
         } else {
-            insert(value);
+            insert_help(value, mRoot->left);
         }
     }
     if (value > mRoot->data){
@@ -79,7 +110,7 @@ size_t Set::insert(const std::string& value){
             mRoot->right = new Node();
             mRoot->right->data = value;
         } else {
-            insert(value);
+            insert_help(value, mRoot->right);
         }
     }
     mRoot->count ++;
