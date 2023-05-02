ed1b3b04307e02f3181e3743e155a023ecacfdba
Skanda Vasishta <skandavasishta@ucsb.edu>
2023-05-02 11:04:07 -0700

    ok

--- setree/Set.cpp
+++ setree/Set.cpp
@@ -6,7 +6,7 @@ Node* copy_set(const Node* root){
     }
 
     Node* copy_node = new Node;
-    copy_node->data = root->data;
+    //copy_node->data = root->data;
     copy_node->count = root->count;
     copy_node->size = root->size;
 
@@ -45,25 +45,21 @@ Set::~Set(){
     
     
 }
-size_t clear_help(Node* node){
-    /*if (node != nullptr){
+void clear_help(Node* root){ //fix here
+    Node * node = new Node;
+    node = root;
+    if (node != nullptr){
         clear_help(node->left);
         clear_help(node->right);
         delete node;
         node = nullptr;
     }
-    */
-    if (node == nullptr){
-        return 0;
-    }
-    size_t count = clear_help(node->left)+1+clear_help(node->right);
-    delete node;
-    return count;
+    //
 }
 
 size_t Set::clear() {
-    size_t tree_size = clear_help(mRoot);
-    mRoot = nullptr;
+    size_t tree_size = count();
+    clear_help(mRoot);
     return tree_size;
 }
 
@@ -72,7 +68,7 @@ bool Set::contains(const std::string& value) const {
     return false;
 }
 
-size_t countNodes(Node* node) {
+size_t countNodes(Node* node) { //fix here
     if (node == nullptr) {
         return 0;
     } else {
@@ -89,40 +85,6 @@ void debug(){
     //ok
 }
 
-/*
-size_t insert_help(const std::string& value, Node*& node){
-    if (node == nullptr){
-        node = new Node;
-        node ->data = value;
-        node->count=1;
-        return node->count;
-    }
-    if (node-> data == value){
-        return 0;
-    } 
-    if (value < node->data){
-        node->count++;
-        return insert_help(value, node->left);
-    }
-    if (value > node->data){
-        node->count++;
-        return insert_help(value, node->right);
-    }
-    return node->count;
-
-}
-size_t Set::insert(const std::string& value){
-    if (mRoot == nullptr){
-        mRoot = new Node;
-        mRoot ->data = value;
-        mRoot->count = 1;
-        return 1;
-    } else {
-        return insert_help(value, mRoot);
-    }
-
-    
-}*/
 size_t Set::insert(const std::string& value) {
     // If the tree is empty, create a new node and make it the root
     if (mRoot == nullptr) {
@@ -168,23 +130,7 @@ const std::string& Set::lookup(size_t n) const{
     return s;
 }
 
-/*void inOrder(Node* root){
-    if (root != nullptr) {
-        if (root->count == 1){
-            inOrder(root->left);
-            std::cout  << root->data ;
-            inOrder(root->right);
-            std::cout << std::endl;
-        } else if (root->count == 0){
-            //
-        } else {
-
-        }
-    } else {
-        //std::cout<<"-";
-    }
-}*/
-void print_helper(Node* root){
+void print_helper(Node* root){ //look here
     if (countNodes(root)==1){
         std::cout << root->data ;
         return;
