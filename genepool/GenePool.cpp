e51ac34d0a856de9e3aabd85be37bdfd041eb744
Skanda Vasishta <skandavasishta@ucsb.edu>
2023-05-07 23:53:57 -0700

    lol

--- genepool/GenePool.cpp
+++ genepool/GenePool.cpp
@@ -1,3 +1,21 @@
 #include "GenePool.h"
 
+
+//Constructor
+GenePool::GenePool(std::istream& stream){
+
+}
+
+//Destructor 
+GenePool:: ~GenePool(){
+
+}
+
 // GenePool Member Functions
+ std::set<Person*> GenePool::everyone() const {
+
+ }
+
+Person* GenePool::find(const std::string& name) const{
+    
+}
--- genepool/Person.cpp
+++ genepool/Person.cpp
@@ -1,3 +1,99 @@
 #include "Person.h"
 
 // Person Member Functions
+ const std::string& Person::name()   const{
+
+ }
+
+  Gender             Person::gender() const{
+
+  }
+  
+  Person*            Person::mother(){
+
+  }
+
+  Person*            Person::father(){
+
+  }
+
+  // Required Relationship Functions
+  std::set<Person*> Person::ancestors(PMod pmod = PMod::ANY){
+
+  }
+
+  std::set<Person*> Person::aunts(PMod pmod = PMod::ANY, SMod smod = SMod::ANY){
+
+  }
+
+  std::set<Person*> Person::brothers(PMod pmod = PMod::ANY, SMod smod = SMod::ANY){
+
+  }
+
+  std::set<Person*> Person::children(){
+
+  }
+
+  std::set<Person*> Person::cousins(PMod pmod = PMod::ANY, SMod smod = SMod::ANY){
+
+  }
+
+  std::set<Person*> Person::daughters(){
+
+  }
+
+  std::set<Person*> Person::descendants(){
+
+  }
+
+  std::set<Person*> Person::grandchildren(){
+
+  }
+
+  std::set<Person*> Person::granddaughters(){
+
+  }
+
+  std::set<Person*> Person::grandfathers(PMod pmod = PMod::ANY){
+
+  }
+
+  std::set<Person*> Person::grandmothers(PMod pmod = PMod::ANY){
+
+  }
+
+  std::set<Person*> Person::grandparents(PMod pmod = PMod::ANY){
+
+  }
+
+  std::set<Person*> Person::grandsons(){
+
+  }
+
+  std::set<Person*> Person::nephews(PMod pmod = PMod::ANY, SMod smod = SMod::ANY){
+
+  }
+
+  std::set<Person*> Person::nieces(PMod pmod = PMod::ANY, SMod smod = SMod::ANY){
+
+  }
+
+  std::set<Person*> Person::parents(PMod pmod = PMod::ANY){
+
+  }
+
+  std::set<Person*> Person::siblings(PMod pmod = PMod::ANY, SMod smod = SMod::ANY){
+
+  }
+
+  std::set<Person*> Person::sisters(PMod pmod = PMod::ANY, SMod smod = SMod::ANY){
+
+  }
+
+  std::set<Person*> Person::sons(){
+
+  }
+
+  std::set<Person*> Person::uncles(PMod pmod = PMod::ANY, SMod smod = SMod::ANY){
+    
+  }
