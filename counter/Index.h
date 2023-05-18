3123100e15a5d803ff6e9b9b3390673f908bca89
Skanda Vasishta <skandavasishta@ucsb.edu>
2023-05-17 22:34:28 -0700

    ok

--- counter/Index.h
+++ counter/Index.h
@@ -7,10 +7,12 @@
 // Space to implement a separate index class, if you choose to do so.
 // This can make things simpler by clearly separating functionality.
 // The Index is in charge of getting pairs from the DataStore by key.
+struct Node;
+
 class Index {
 private:
     int size = 10000;
-    struct Node** hashTable;
+     Node** hashTable;
 
 public:
     Index();
