deb4d6f3baae3489fc59c464906b3c67850e3225
Skanda Vasishta <skandavasishta@ucsb.edu>
2023-05-19 01:27:17 -0700

    please

--- counter/Iterator.cpp
+++ counter/Iterator.cpp
@@ -18,10 +18,10 @@ int Counter::Iterator::value() const{
 }
 
  Counter::Iterator& Counter::Iterator::operator ++ (){
-    if (currentNode == nullptr) {
-        return;
+    if (currentNode) {
+        currentNode = currentNode->next;
     }
-    currentNode = currentNode->next;
+    //currentNode = currentNode->next;
     return *this;
  }
  bool      Counter::Iterator::operator == (const Iterator& other) const{
