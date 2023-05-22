aa92af5606c99dc182cbfc0f9012b27702efa899
Skanda Vasishta <skandavasishta@ucsb.edu>
2023-05-21 17:41:58 -0700

    ok

---
+++ typo/Heap.cpp
@@ -0,0 +1,46 @@
+#include "Heap.h"
+#include <iostream>
+
+Heap::Heap(size_t capacity){
+
+}
+
+Heap::Heap(const Heap& other){
+
+}
+
+Heap::Heap(Heap&& other){
+
+}
+
+Heap::~Heap(){
+
+}
+
+size_t Heap::capacity() const{
+    return 1;
+}
+
+size_t Heap::count() const{
+    return 1;
+}
+
+const Heap::Entry& Heap::lookup(size_t index) const{
+    return *mData;
+}
+
+Heap::Entry Heap::pop(){
+    return *mData;
+}
+
+Heap::Entry Heap::pushpop(const std::string& value, float score){
+    return *mData;
+}
+
+void Heap::push(const std::string& value, float score){
+
+}
+
+const Heap::Entry& Heap::top() const{
+    return *mData;
+}
---
+++ typo/WordList.cpp
@@ -0,0 +1,8 @@
+#include "WordList.h"
+WordList::WordList(std::istream& stream){
+
+}
+
+Heap WordList::correct(const std::vector<Point>& points, size_t maxcount, float cutoff) const{
+    
+}
