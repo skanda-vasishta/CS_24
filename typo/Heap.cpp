98ad1cc921726fd03738c3b85565fa109f9d2c16
Skanda Vasishta <skandavasishta@ucsb.edu>
2023-05-22 16:27:06 -0700

    ok

--- typo/Heap.cpp
+++ typo/Heap.cpp
@@ -51,10 +51,10 @@ const Heap::Entry& Heap::lookup(size_t index) const{
 }
 void pop_help(size_t index, size_t count, Heap::Entry* other){
     size_t small = index;
-    if (((index*2)+1) < count-1 && other[(index*2)+1].score < other[index].score){
+    if (((index*2)+1) <= count && other[(index*2)+1].score < other[index].score){
         small = (index*2) + 1;
     }
-    if (((index*2)+2) < count-1 && other[(index*2)+2].score < other[index].score){
+    if (((index*2)+2) <= count && other[(index*2)+2].score < other[index].score){
         small = (index*2) + 2;
     }
     if (small != index){
