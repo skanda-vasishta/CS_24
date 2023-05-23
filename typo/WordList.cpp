8230779c909bf85fd59760b6a7d382cd522b06bc
Skanda Vasishta <skandavasishta@ucsb.edu>
2023-05-23 13:39:59 -0700

    ok

--- typo/WordList.cpp
+++ typo/WordList.cpp
@@ -1,8 +1,23 @@
 #include "WordList.h"
 WordList::WordList(std::istream& stream){
-
+    //parse stream into mWords
+    std::string stream_string;
+    while (stream>>stream_string)
+    {
+        bool lower = true;
+        for (int i = 0; i < stream_string.size(); i ++){
+            if (!islower(stream_string[i])){
+                lower = false;
+                break;
+            }
+        }
+        if (lower){
+            mWords.push_back(stream_string);
+        }
+    }
+    
 }
 
 Heap WordList::correct(const std::vector<Point>& points, size_t maxcount, float cutoff) const{
-    
+    return Heap(maxcount);
 }
