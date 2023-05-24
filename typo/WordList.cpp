c7cdb163923f6a0bff2bc0e9064433b1474fbf97
Skanda Vasishta <skandavasishta@ucsb.edu>
2023-05-23 17:56:58 -0700

    ok

--- typo/WordList.cpp
+++ typo/WordList.cpp
@@ -27,13 +27,18 @@ Heap WordList::correct(const std::vector<Point>& points, size_t maxcount, float 
     // then score using euclidean distance and scoring equation 
     Heap wordlist = Heap(maxcount);
     int mLen = mWords.size();
+    int pLen = points.size();
+    
     for (int i = 0; i < mLen; i++){
         float total = 0;
         int strLen = mWords[i].length();
+        if (strLen != pLen) {
+            continue;
+        }
         for (int j = 0; j < strLen; j++){
             int index = mWords[i][j] - 65;
             Point alphabet = QWERTY[index];
-            float euclid = sqrt(pow(alphabet.x - points.at(i).x, 2)+pow(alphabet.y - points.at(i).y, 2));
+            float euclid = sqrt(pow(alphabet.x - points.at(j).x, 2)+pow(alphabet.y - points.at(j).y, 2));
             float score = 1/(10 * pow(euclid, 2) +1);
             total+=score;
         }
