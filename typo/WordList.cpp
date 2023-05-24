6304992fbca7977aad4bb938d87700d713d79d8b
Skanda Vasishta <skandavasishta@ucsb.edu>
2023-05-24 00:32:31 -0700

    ok

--- typo/WordList.cpp
+++ typo/WordList.cpp
@@ -45,7 +45,9 @@ Heap WordList::correct(const std::vector<Point>& points, size_t maxcount, float 
         float avg = total/strLen;
         if (avg > cutoff){
             if (wordlist.count() == maxcount){
+                if (avg > wordlist.top().score){
                 wordlist.pushpop(mWords[i], avg);
+                }
             } else {
                 wordlist.push(mWords[i], avg);
             } 
