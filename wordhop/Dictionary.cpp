c97925aa59f8faa8b79ff6e0c98941e51999be9d
Skanda Vasishta <skandavasishta@ucsb.edu>
2023-05-31 14:04:50 -0700

    ok

--- wordhop/Dictionary.cpp
+++ wordhop/Dictionary.cpp
@@ -1,16 +1,4 @@
 #include "Dictionary.h"
- //yoo ok sick this actually works
-
- //light the beam 
-
-//  Initialize an empty set to keep track of visited words in the chain.
-// Start with the source word and add it to the visited set.
-// Generate all possible valid next words by changing one letter in the current word.
-// For each valid next word:
-// If the next word is the destination word, you have found a valid chain. Stop and return the chain.
-// If the next word has already been visited, skip it to avoid loops.
-// Otherwise, add the next word to the visited set and recursively continue the chain-building process from the next word.
-// If no valid chain is found after exploring all possible next words, throw a NoChain exception.
 
 
 Dictionary::Dictionary(std::istream& stream) {
@@ -54,7 +42,7 @@ std::unordered_set<std::string> Dictionary::validwords(const std::string& word){
                 continue;
             }
             mWord[i] = l;
-            if (newWords.find(mWord) == newWords.end()){
+            if (newWords.find(mWord) == newWords.end() && words_.find(mWord)!= words_.end()){
                 newWords.insert(mWord);
             }
         }
@@ -63,108 +51,58 @@ std::unordered_set<std::string> Dictionary::validwords(const std::string& word){
     return newWords;
 }
 
+std::vector<std::string> Dictionary::hop(const std::string& from, const std::string& to){
 
-std::vector<std::string> Dictionary::hop(
-  const std::string& from, const std::string& to) {
     if (from.length() != to.length()) {
         throw NoChain();
-    }
-    if (from == to) {
-        std::vector<std::string> chain;
-        chain.push_back(from);
-        return chain;
-    }
-
-    std::unordered_set<std::string> visitedFrom; // Visited set for forward search
-    std::unordered_set<std::string> visitedTo; // Visited set for backward search
-    std::unordered_map<std::string, std::string> prevFrom; // Previous word map for forward search
-    std::unordered_map<std::string, std::string> prevTo; // Previous word map for backward search
-
-    std::queue<std::string> queueFrom; // Queue for forward search
-    std::queue<std::string> queueTo; // Queue for backward search
-
-    queueFrom.push(from);
-    queueTo.push(to);
-
-    visitedFrom.insert(from);
-    visitedTo.insert(to);
-
-    std::string meetingWord = "";
-
-    while (!queueFrom.empty() && !queueTo.empty()) {
-        // Forward search
-        std::string currentWordFrom = queueFrom.front();
-        queueFrom.pop();
-
-        const std::unordered_set<std::string>& validWordsFrom = validwords(currentWordFrom);
-
-        for (const std::string& word : validWordsFrom) {
-            if (visitedTo.find(word) != visitedTo.end()) {
-                meetingWord = word;
-                prevFrom[word] = currentWordFrom;
-                break;
-            }
-
-            if (visitedFrom.find(word) == visitedFrom.end()) {
-                queueFrom.push(word);
-                visitedFrom.insert(word);
-                prevFrom[word] = currentWordFrom;
-            }
         }
-
-        if (!meetingWord.empty()) {
-            break;
-        }
-
-        // Backward search
-        std::string currentWordTo = queueTo.front();
-        queueTo.pop();
-
-        const std::unordered_set<std::string>& validWordsTo = validwords(currentWordTo);
-
-        for (const std::string& word : validWordsTo) {
-            if (visitedFrom.find(word) != visitedFrom.end()) {
-                meetingWord = word;
-                prevTo[word] = currentWordTo;
-                break;
-            }
-
-            if (visitedTo.find(word) == visitedTo.end()) {
-                queueTo.push(word);
-                visitedTo.insert(word);
-                prevTo[word] = currentWordTo;
-            }
-        }
-
-        if (!meetingWord.empty()) {
-            break;
-        }
+    if (from == to){
+      std::vector<std::string> chain;
+      chain.push_back(from);
+      // chain.push_back(to);
+      return chain;
     }
 
-    if (!meetingWord.empty()) {
-        std::vector<std::string> wordChainFrom;
-        std::string word = meetingWord;
+    std::queue<std::string>wordQueue;
+    wordQueue.push(from);
 
-        while (!word.empty()) {
-            wordChainFrom.push_back(word);
-            word = prevFrom[word];
+    std::unordered_map<std::string, std::string> prevWordMap;
+    prevWordMap[from] = "";
+
+    std::unordered_map<std::string, std::unordered_set<std::string>> validWordsMap;
+
+    while (!wordQueue.empty()) {
+        std::string currentWord = wordQueue.front();
+        wordQueue.pop();
+
+        if (validWordsMap.find(currentWord) == validWordsMap.end()) {
+            validWordsMap[currentWord] = validwords(currentWord);
         }
+        const std::unordered_set<std::string>& validWords = validWordsMap[currentWord];
 
-        std::reverse(wordChainFrom.begin(), wordChainFrom.end());
+        for (const std::string& word : validWords) {
+            if (word == to) {
+                std::vector<std::string> wordChain;
+                wordChain.push_back(to);
 
-        std::vector<std::string> wordChainTo;
-        word = meetingWord;
+                std::string prevWord = currentWord;
+                while (!prevWord.empty()) {
+                    wordChain.push_back(prevWord);
+                    prevWord = prevWordMap[prevWord];
+                }
 
-        while (prevTo[word] != to) {
-            word = prevTo[word];
-            wordChainTo.push_back(word);
+                std::reverse(wordChain.begin(), wordChain.end());
+                return wordChain;
+            }
+
+            if (prevWordMap.find(word) == prevWordMap.end()) {
+                wordQueue.push(word);
+                prevWordMap[word] = currentWord;
+            }
         }
-
-        std::reverse(wordChainTo.begin(), wordChainTo.end());
-
-        wordChainFrom.insert(wordChainFrom.end(), wordChainTo.begin(), wordChainTo.end());
-        return wordChainFrom;
     }
 
     throw NoChain();
+
+
 }
