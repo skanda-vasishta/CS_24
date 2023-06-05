42724b8f4e7c74d440d01267eb7cb4f08e89eeb6
Skanda Vasishta <skandavasishta@ucsb.edu>
2023-06-05 13:44:50 -0700

    ok

--- lineup/Database.cpp
+++ lineup/Database.cpp
@@ -5,3 +5,24 @@
 Database* Database::create() {
   return new Database();
 }
+
+Database::Database(){
+
+}
+
+Database::~Database(){
+  
+}
+
+void Database::insert(const Report* report){
+
+}
+
+std::vector<const Report*> Database::search(float age, float height, float weight) const {
+  std::vector<const Report*> temp;
+  return temp;
+}
+
+void Database::remove(unsigned int id){
+
+}
