38be1634ed40992c6c4eba537d998bf8ce6cc15c
Skanda Vasishta <skandavasishta@ucsb.edu>
2023-06-05 16:21:02 -0700

    ok

--- lineup/Database.cpp
+++ lineup/Database.cpp
@@ -27,6 +27,14 @@ void Database::insert(const Report* report){
 
 std::vector<const Report*> Database::search(float age, float height, float weight) const {
   std::vector<const Report*> found;
+  for (auto i : database){
+    if (age > i->age.min && age < i->age.max && 
+        height > i->height.min && height < i->height.max &&
+        weight > i->weight.min && weight < i->weight.max) 
+      {
+        found.push_back(i);
+      }
+  }
 
   return found;
 }
